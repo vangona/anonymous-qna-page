7d4226e1a04f4b62fc8b412479e8c05991e2f01b
vangona <rlarhksrud12@naver.com>
2021-08-04 16:30:13 +0900

    2021.08.04 js koalanlp try

--- src/components/Wordcloud.js
+++ src/components/Wordcloud.js
@@ -1,8 +1,37 @@
 import React, { useEffect } from "react";
 import * as d3 from "d3";
 import cloud from "d3-cloud";
+import mod from "korean-text-analytics"
 
 const Wordcloud = () => {
+  const nlp = () => {
+    const task = new mod.TaskQueue();
+
+    mod.ExecuteMorphModule('아버지가 방에 들어가신다,', function(err, rep) {
+      console.log(err, rep);
+    })
+  }
+
+  const end = (words) => {
+    d3.select("#word-cloud")
+        .append("svg")
+        .attr("width", 500)
+        .attr("height", 500)
+        .style("border", "1px solid black")
+      .append("g")
+        .attr("transform", "translate(" + 500 / 2 + "," + 500 / 2 + ")")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+  }
+
     useEffect(() => {
         const data = [
             "Hello", "world", "normally", "you", "want", "more", "words",
@@ -17,26 +46,9 @@ const Wordcloud = () => {
         .fontSize(function(d) { return d.size; })
         .on("end", end)
         .start();
+
+        nlp();
     })
-    function end(words) {
-        d3.select("#word-cloud")
-            .append("svg")
-            .attr("width", 500)
-            .attr("height", 500)
-            .style("border", "1px solid black")
-          .append("g")
-            .attr("transform", "translate(" + 500 / 2 + "," + 500 / 2 + ")")
-          .selectAll("text")
-            .data(words)
-          .enter().append("text")
-            .style("font-size", function(d) { return d.size + "px"; })
-            .style("font-family", "Impact")
-            .attr("text-anchor", "middle")
-            .attr("transform", function(d) {
-              return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-            })
-            .text(function(d) { return d.text; });
-      }
   
     return (
       <div>
