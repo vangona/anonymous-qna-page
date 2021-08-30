17a74cc2f2f20f7cc26a11287b43438fac11b112
vangona <rlarhksrud12@naver.com>
2021-08-30 15:04:47 +0900

    2021.08.30

--- src/components/Wordcloud.js
+++ src/components/Wordcloud.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import * as d3 from "d3";
 import cloud from "d3-cloud";
+import { KMR, KKMA } from "koalanlp/API";
+import { initialize } from 'koalanlp/Util';
+import { Tagger, Parser } from 'koalanlp/proc';
 
 const Wordcloud = () => {
-
   
-  const getData = () => {
-    
-  }
+  // const getData = async () => {
+  //   await initialize({packages: {KMR: '2.0.4', KKMA: '2.0.4'}, verbose: true})
+  // }
 
   const end = (words) => {
     d3.select("#word-cloud")
@@ -29,28 +31,33 @@ const Wordcloud = () => {
         .text(function(d) { return d.text; });
   }
 
-    useEffect(() => {
-        const data = [
-            "Hello", "world", "normally", "you", "want", "more", "words",
-            "than", "this"]
-        cloud()
-        .size([400, 400])
-        .words(data.map(function(d) {
-            return {text: d, size: 10 + Math.random() * 90, test: "haha"};
-        }))
-        .padding(5)
-        .font("Impact")
-        .fontSize(function(d) { return d.size; })
-        .on("end", end)
-        .start();
-    })
-  
-    return (
-      <div>
-        <h1>리뷰 분석 결과</h1>
-        <div id="word-cloud"></div>
-      </div>
-    )
+    const makeCloud = data => {
+      cloud()
+      .size([400, 400])
+      .words(data.map(function(d) {
+          return {text: d, size: 10 + Math.random() * 90, test: "haha"};
+      }))
+      .padding(5)
+      .font("Impact")
+      .fontSize(function(d) { return d.size; })
+      .on("end", end)
+      .start();
+    }
+
+  useEffect(() => {
+    // getData();
+    const data = [
+        "Hello", "world", "normally", "you", "want", "more", "words",
+        "than", "this"]
+    makeCloud(data)
+  })
+
+  return (
+    <div>
+      <h1>리뷰 분석 결과</h1>
+      <div id="word-cloud"></div>
+    </div>
+  )
 }
 
 export default Wordcloud;
